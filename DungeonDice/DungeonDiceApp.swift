00a5bac794f3818658b55c450fb5954a9692257a
TheodoreUtomo <theodoreutomo@gmail.com>
2024-09-26 15:34:09 -0400

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Theodore Utomo on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DungeonDiceTests/DungeonDiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceTests.swift
+//  DungeonDiceTests
+//
+//  Created by Theodore Utomo on 9/26/24.
+//
+
+import Testing
+@testable import DungeonDice
+
+struct DungeonDiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
