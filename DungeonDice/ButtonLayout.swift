61c6963231bbede03eda6feb5f4a3d9275efd7e6
TheodoreUtomo <theodoreutomo@gmail.com>
2024-09-30 15:36:13 -0400

    Made code more modular

---
+++ DungeonDice/ButtonLayout.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonLayout.swift
+//  DungeonDice
+//
+//  Created by Theodore Utomo on 9/29/24.
+//
+
+import SwiftUI
+
+struct ButtonLayout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonLayout()
+}
